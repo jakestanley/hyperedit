3b01ad9d25fd887510a8d1c853a9f0341fcb8959
Jake Stanley <prudentia713@gmail.com>
2024-06-04 00:17:29 +0100

    srt edit now modifies original after backing up original to a timestamped file

--- he_srt_editor.py
+++ he_srt_editor.py
@@ -3,14 +3,14 @@ import subprocess
 import os
 
 from py.args import parseSrtEditorArgs
-from py.srt import parse_srt, write_srt, create_srt_entry
+from py.srt import parse_srt, write_srt, create_srt_entry, merge, backup_srt
 
 def replace_or_add_srt_entry(srt_entries, srt_id, new_start, new_end, new_text):
     entry_found = False
     for i in range(len(srt_entries)):
         entry = srt_entries[i]
         if int(entry[0]) == srt_id:
-            srt_entries[i] = create_srt_entry(srt_id, new_start, new_end, new_text)
+            srt_entries[i] = [srt_id, new_start, new_end]
             entry_found = True
             break
     
@@ -33,6 +33,9 @@ if not args.command:
 srt_file_path = args.srt_file_path
 srt_id = args.srt_id
 
+if os.path.exists(srt_file_path) == False:
+    raise Exception(f"SRT file {srt_file_path} does not exist")
+
 srts = parse_srt(srt_file_path)
 if len(srts) == 0:
     raise Exception("SRT file is empty")
@@ -52,21 +55,24 @@ if args.command != "remove":
     duration = end - start
 
 if args.command == "edit" or args.command == "remove":
-    directory = os.path.dirname(args.srt_file_path)
-    basename, _ = os.path.splitext(args.srt_file_path)
-    srt_overrides_path = f"{basename}-overrides.srt"
-
-    if os.path.exists(srt_overrides_path):
-        srt_entries = parse_srt(srt_overrides_path)
-    else:
-        srt_entries = []
+    basename, _ = os.path.splitext(srt_file_path)
 
     if args.command == "edit":
-        replace_or_add_srt_entry(srt_entries, srt_id, start, end, 'text unused')
+        replace_or_add_srt_entry(srts, srt_id, start, end, 'text unused')
     else:
-        remove_srt_entry(srt_entries, srt_id)
+        remove_srt_entry(srts, srt_id)
     
-    write_srt(srt_overrides_path, srt_entries)
+    merged_srts = merge(srts)
+    formatted_srts = []
+    # srt_id = 1
+    for srt_id, start, end in merge(srts):
+        formatted_srts.append(create_srt_entry(srt_id=srt_id, start=start, end=end))
+        # NOTE: avoiding doing this. reindexing SRTs is done in deaggress but 
+        #   here it would result in loads of SRTs needing to be re-rendered
+        # srt_id += 1
+
+    backup_srt(srt_file_path)
+    write_srt(srt_file_path, formatted_srts)
 elif args.command == "remove":
     raise("Not yet implemented")
 elif args.command == "preview":
