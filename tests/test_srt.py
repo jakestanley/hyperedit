487291edcf45e71cd20067ad58fc6ee3af670780
Jacob Stanley <>
2024-06-10 18:03:42 +0100

    fixed old srt test i'd forgotten about and floating point test output bollocks

--- tests/test_srt.py
+++ tests/test_srt.py
@@ -1,7 +1,7 @@
 import unittest
 
 from parameterized import parameterized
-from py.srt import seconds_to_srt_timestamp, deaggress_ranges_by_seconds
+from hyperedit.srt import seconds_to_srt_timestamp, deaggress_ranges_by_seconds
 
 class TestSrt(unittest.TestCase):
 
@@ -13,7 +13,14 @@ class TestSrt(unittest.TestCase):
         self.assertEqual(seconds_to_srt_timestamp(input), expected)
 
     @parameterized.expand([
-        [[('1', 4.32, 6.605)], 0.5, [('1', 3.82, 7.105)]],
+        [[('1', 4.32, 6.605)], 0.5, 
+         [('1', 3.82, 6.855)]],
     ])
     def test_deaggress(self, input_time, input_seconds, expected):
-        self.assertEqual(deaggress_ranges_by_seconds(input_time, input_seconds), expected)
+        # end seconds is always halved input seconds so it doesn't overlap with next
+        actual = deaggress_ranges_by_seconds(input_time, input_seconds)
+        # assert ID (string)
+        self.assertEqual(actual[0][0], expected[0][0])
+        # assert start and end times (floats)
+        self.assertAlmostEqual(actual[0][1], expected[0][1])
+        self.assertAlmostEqual(actual[0][2], expected[0][2])
