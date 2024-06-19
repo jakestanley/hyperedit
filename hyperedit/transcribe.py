d18c03e23db2fa92500a1a217defa8fe32c964ee
Jake Stanley <prudentia713@gmail.com>
2024-06-19 17:31:31 +0100

    optional to_file argument for transcribe

--- hyperedit/transcribe.py
+++ hyperedit/transcribe.py
@@ -21,13 +21,16 @@ def _do_transcribe(input_path, output_path):
         raise("Error generating SRT:")
         # print(result.stderr.decode())
 
-def transcribe(audio_file_path=None):
+def transcribe(audio_file_path=None, to_file=None):
 
     if audio_file_path is None:
         raise Exception("Application error: 'audio_file_path' argument missing")
 
-    basename, _ = os.path.splitext(audio_file_path)
-    output_file_path = f"{basename}.srt"
+    if to_file:
+        output_file_path = to_file
+    else:
+        basename, _ = os.path.splitext(audio_file_path)
+        output_file_path = f"{basename}.srt"
 
     # TODO: option to overwrite
     if os.path.exists(output_file_path):
