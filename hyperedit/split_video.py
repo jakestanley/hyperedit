54f0f57a4af8c175e05950d4f7584344267a49fe
Jake Stanley <prudentia713@gmail.com>
2024-07-25 00:01:12 +0100

    refactor split_video into split and concat

--- hyperedit/split_video.py
+++ hyperedit/split_video.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import time
 
-from hyperedit.srt import parse_srt, seconds_to_srt_timestamp, GetPrimitiveSrtListHash
+from hyperedit.srt import seconds_to_srt_timestamp, GetPrimitiveSrtListHash
 from hyperedit.ffmpeg import get_params_for_gpu, seconds_to_ffmpeg_timestamp as stff
 from hyperedit.time import seconds_to_output_timestamp, seconds_to_time_remaining
 from hyperedit.concatenate import create_file_list, concatenate_clips
@@ -10,14 +10,22 @@ from hyperedit.concatenate import create_file_list, concatenate_clips
 def _escape_text(text):
     return text.replace(":", r'\:').replace(",", r'\,').replace("'", r"\'")
 
-def _generate_ffmpeg_commands(video_file, time_ranges, output_prefix, gpu, preview=False, overwrite=False, srt_file_path=None):
+def _generate_split_ffmpeg_commands(
+        srts=[],
+        original_video_file_path=None, 
+        output_directory=None, 
+        preview=False, 
+        overwrite=False,
+        gpu=None
+        ):
 
     gpu_params = get_params_for_gpu(gpu)
+    srt_hash = GetPrimitiveSrtListHash(srts)
 
     commands = []
     output_files = []
     srt_ids = []
-    for srt_id, start, end, _ in time_ranges:
+    for srt_id, start, end, _ in srts:
         formatted_start = seconds_to_output_timestamp(start)
         formatted_end = seconds_to_output_timestamp(end)
         seek_time = start - 2
@@ -26,13 +34,15 @@ def _generate_ffmpeg_commands(video_file, time_ranges, output_prefix, gpu, previ
         seek_offset = start - seek_time
         duration = end - start
 
-
         filter_complex = []
         maps = []
 
+        output_file = os.path.join(output_directory, f"{"preview" if preview else "final"}_S{srt_id}_{formatted_start}_to_{formatted_end}.mp4")
+        preset = gpu_params['fast_preset'] if preview else gpu_params['quality_preset']
+
+        # if we're in preview mode, render at lower detail with "debug" text
         if preview:
-            output_file = f"{output_prefix}_S{srt_id}_{formatted_start}_to_{formatted_end}.mp4"
-            preset = gpu_params['fast_preset']
+
             # scale video
             filter_complex.append(f"[0:v]scale=-1:480[v_scaled]")
 
@@ -42,29 +52,28 @@ def _generate_ffmpeg_commands(video_file, time_ranges, output_prefix, gpu, previ
             filter_complex.append(f"[v_scaled]{drawtext}[v_overlaid1]")
 
             # preview text 2
-            if srt_file_path:
-                preview_text_2 = _escape_text(f"SRT file: {os.path.basename(srt_file_path)}")
+            # TODO: hyperedit-gui should save the SRTs with a hash for later
+            if srt_hash:
+                preview_text_2 = _escape_text(f"SRT hash: {srt_hash}")
                 drawtext = f"drawtext=text='{preview_text_2}':fontsize=12:fontcolor=white:box=1:boxcolor=black@0.5:boxborderw=2:x=10:y=40"
                 filter_complex.append(f"[v_overlaid1]{drawtext}[v_overlaid2]")
                 maps.append('[v_overlaid2]')
             else:
                 maps.append('[v_overlaid1]')
-            
-            maps.append('0:a')
 
         else:
-            output_file = f"{output_prefix}_S{srt_id}_{formatted_start}_to_{formatted_end}.mp4"
-            preset = gpu_params['quality_preset']
             # map all video and audio streams
             maps.append('0:v')
-            maps.append('0:a')
+            
+        # always append all audio streams
+        maps.append('0:a')
 
         if not os.path.exists(output_file) or overwrite:
             cmd = [
                 'ffmpeg', '-y',
                 # '-hwaccel', hwaccel, # this doesn't work on mac OR windows right now
                 '-ss', stff(seek_time),
-                '-i', video_file, 
+                '-i', original_video_file_path, 
                 '-ss', stff(seek_offset),
                 '-t', stff(duration), 
                 '-c:v', gpu_params['encoder'], 
@@ -84,6 +93,7 @@ def _generate_ffmpeg_commands(video_file, time_ranges, output_prefix, gpu, previ
             commands.append(cmd)
             srt_ids.append(srt_id)
         output_files.append(output_file)
+
     return commands, output_files, srt_ids
 
 def _run_ffmpeg_commands(commands, srt_ids):
@@ -101,58 +111,35 @@ def _run_ffmpeg_commands(commands, srt_ids):
             print(f" {duration:.1f} seconds. Rolling average: {sum(durations)/len(durations):.1f} seconds. ETA: {seconds_to_time_remaining((sum(durations)/len(durations))*(len(commands)-i))}\r", end='', flush=True)
     return durations
 
-def split_video(
-        srt_file_path=None,
-        srts=None,
-        video_file_path=None,
+def split(
+        srts: list[list[str]]=None, # TODO is this type hint correct?
+        original_video_file_path=None,
+        output_directory=None,
         preview=False,
         overwrite=False,
-        range=None,
-        gpu=None,
-        output_directory=None
+        gpu=None
         ):
     
-    if srt_file_path is None:
-        if srts:
-            time_ranges = srts
-        else:
-            raise Exception("One of arguments srt_file_path or srts are required")
-    else:
-        time_ranges = parse_srt(srt_file_path)
+    if not srts:
+        raise Exception("Argument 'srts' is required")
 
-    if video_file_path is None:
-        raise Exception("Video file path is required")
-    
-    # TODO test required args are present or raise exception
+    if original_video_file_path is None:
+        raise Exception("Argument 'original_video_file_path' is required")
 
+    # for hyperedit this should be project-path/CLIPS
     if not output_directory:
-    # if output directory not provided, put the clips in the same directory as the source video file
-        if preview:
-            output_prefix = f"{os.path.splitext(video_file_path)[0]}_preview"
-        else:
-            output_prefix, _ = os.path.splitext(video_file_path)
-    else:
-    # if output directory is provided, put the clips in the provided directory
-        if preview:
-            output_prefix = os.path.join(output_directory, "preview")
-        else:
-            output_prefix = output_directory
-
-    # TODO use args to generate this instead of just using a timestamp
-    list_filename = f'file_list_{int(time.time())}.txt'
-
-    # TODO ignore if srts provided
-    if srts:
-        final_output = f"{output_prefix}_final_{GetPrimitiveSrtListHash(srts)}.mp4"
-    elif range:
-        final_output = f"{output_prefix}_final_{range[0]}-{range[1]}.mp4"
-        time_ranges = parse_srt(srt_file_path)[(range[0]-1):(range[1]-1)]
-    else:
-        final_output = f"{output_prefix}_final.mp4"
+        # if not provided, create a directory with the file path name and put it in CLIPS
+        output_directory = os.path.join(os.path.dirname(original_video_file_path), os.path.basename(original_video_file_path), "CLIPS")
 
     # generate ffmpeg commands 
-    # TODO separate output_files and srt_ids generation so concat can go in its own file
-    ffmpeg_commands, output_files, srt_ids = _generate_ffmpeg_commands(video_file_path, time_ranges, output_prefix, gpu, preview, overwrite, srt_file_path)
+    #ffmpeg_commands, output_files, srt_ids = _generate_split_ffmpeg_commands(original_video_file_path, srts, output_prefix, gpu, preview, overwrite)
+    ffmpeg_commands, output_files, srt_ids = _generate_split_ffmpeg_commands(
+        srts=srts,
+        original_video_file_path=original_video_file_path, 
+        output_directory=output_directory,
+        preview=preview,
+        overwrite=overwrite,
+        gpu=gpu)
 
     # Run the FFmpeg commands to split the video
     if len(ffmpeg_commands) == 0:
@@ -164,15 +151,32 @@ def split_video(
         print(f"Splitting took {end_time - start_time:.1f} seconds")
         print(f"Average split time: {sum(durations)/len(durations):.1f} seconds")
 
+    return output_files
+
+def concat(
+        srts: list[list[str]] = [],
+        original_video_file_path=None,
+        output_directory=None,
+        preview=False,
+        overwrite=False, # TODO use this parameter
+        gpu=None,
+        files=[]):
+    
+    if output_directory is None:
+        output_directory = os.path.join(os.path.dirname(original_video_file_path), os.path.basename(original_video_file_path), "RENDERS")
+
+    final_output = os.path.join(output_directory, f"{"preview" if preview else "final"}_{GetPrimitiveSrtListHash(srts)}.mp4")
+    list_filename = f'file_list_{int(time.time())}.txt'
+
     # Create the file list for concatenation
-    create_file_list(output_files, list_filename)
+    create_file_list(files, list_filename)
 
     # Concatenate the split clips into a single video
     start_time = time.time()
     concatenate_clips(list_filename, final_output, gpu)
     end_time = time.time()
+    
     print(f"Concatenation took {end_time - start_time:.1f} seconds")
-
-    print("Final video has been successfully generated.")
+    print(f"{"Preview" if preview else "Final"} video has been successfully generated.")
 
     return final_output
